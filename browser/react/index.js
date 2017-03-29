7ee9dd46f38eadde7aa5b540a2dbe49c8b55389d
fourestfire <kewliez@gmail.com>
2017-03-29 11:21:04 -0400

    working on artist page

--- browser/react/index.js
+++ browser/react/index.js
@@ -4,15 +4,22 @@ import {Router, Route, IndexRedirect, hashHistory, browserHistory, Link} from 'r
 import AppContainer from './containers/AppContainer';
 import Albums from './components/Albums';
 import Album from './components/Album';
+import Artists from './components/Artists';
+import Artist from './components/Artist';
+import ArtistSongs from './components/ArtistSongs';
+import ArtistAlbums from './components/ArtistAlbums';
 
 ReactDOM.render(
   <Router history = {hashHistory}>
-
-  	<Route path='/' component = {AppContainer} >
-  		<IndexRedirect to = '/albums'/>
-  		<Route path='/albums' component = {Albums} />
-		<Route path = '/albums/:albumId' component = {Album} />
-  	</Route>
+		<Route path='/' component = {AppContainer} >
+				<IndexRedirect to = '/albums'/>
+				<Route path='/albums' component = {Albums} />
+				<Route path = '/albums/:albumId' component = {Album} />
+				<Route path='/artists' component = {Artists} />
+				<Route path = '/artists/:artistId' component = {Artist} />
+				<Route path = '/artists/:artistId/albums' component = {Albums} />
+				<Route path = '/artists/:artistId/songs' component = {ArtistSongs} />
+		</Route>
   </Router>,
 
   document.getElementById('app')
